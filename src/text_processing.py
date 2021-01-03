b80765c4b434dc81227c9fd451fb4453cd563339
Adam Osiowy <adamosiowy@wp.pl>
2021-01-03 15:10:58 +0100

    poprawa procesu przetwarzania polecen

--- src/text_processing.py
+++ src/text_processing.py
@@ -11,9 +11,8 @@ def tokenize(text):
 
 def checkIfContainOrder(text) -> bool:
     tokens = tokenize(text)
-    for tok in tokens:
-        if tok.tag_ == "IMPT":
-            return True
+    if tokens[0].tag_ == "IMPT":
+        return True
     return False
 
 
@@ -35,33 +34,16 @@ def removePunctuationMarks(tokens):
     return [re.sub(r'[?:!,.;]', r'', tok) for tok in tokens]
 
 
-def getTaskAndArgs(text: str):
-    tokens = tokenize(text)
-    lemmas = lematize(tokens)
-    withoutStopWords = removeStopWords(lemmas)
-    withoutPunctuationMarks = removePunctuationMarks(withoutStopWords)
-    new_tokens = tokenize(' '.join(withoutPunctuationMarks))
-    parts_of_speech = tagPartOfSpeech(new_tokens)
-    print(parts_of_speech)
-
-
-if __name__ == "__main__":
-    doc1 = 'wyłącz Spotify'
-    doc2 = 'zmniejsz głośność odtwarzacza muzyki'
-    doc3 = 'wyszukaj w internecie informacje o drugiej wojnie światowej'
-    doc4 = 'zrób zrzut ekranu i zapisz go jako screenshot1'
-    doc5 = 'zapisać zapisz'
-
-    # for token in doc1:
-    #     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-    #           token.shape_, token.is_alpha, token.is_stop)
-    getTaskAndArgs(doc1)
-    checkIfContainOrder(doc1)
-    getTaskAndArgs(doc2)
-    checkIfContainOrder(doc2)
-    getTaskAndArgs(doc3)
-    checkIfContainOrder(doc3)
-    getTaskAndArgs(doc4)
-    checkIfContainOrder(doc4)
-    getTaskAndArgs(doc5)
-    checkIfContainOrder(doc5)
+def getTaskAndArgs(text: str) -> dict:
+    if checkIfContainOrder(text):
+        tokens = tokenize(text)
+        lemmas = lematize(tokens)
+        withoutStopWords = removeStopWords(lemmas)
+        withoutPunctuationMarks = removePunctuationMarks(withoutStopWords)
+        new_tokens = tokenize(' '.join(withoutPunctuationMarks))
+        order = new_tokens[0]
+        new_tokens = new_tokens[1:]
+        parts_of_speech = tagPartOfSpeech(new_tokens)
+        return {'order': order, 'args': parts_of_speech}
+    else:
+        return {'order': '', 'args': dict()}
