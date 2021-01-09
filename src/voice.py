a15f5d0135a0264a7d2120e4c26eb8de41650843
Adam Osiowy <adamosiowy@wp.pl>
2021-01-09 23:10:33 +0100

    deleted stop_recording func

--- src/voice.py
+++ src/voice.py
@@ -2,25 +2,20 @@ import speech_recognition as sr
 import winsound
 
 
-def recognizeVoice(recognizer, audio):
-    try:
-        result = recognizer.recognize_google(audio, language='pl-PL')
-        return result
-    except LookupError:
-        return ''
-
-
-def startRecording():
+def recognizeVoice():
     r = sr.Recognizer()
-    m = sr.Microphone(device_index=1)
-    global stop_listening
-    stop_listening = r.listen_in_background(m, recognizeVoice)
-
-
-def stopRecording():
-    global stop_listening
-    stop_listening()
+    # print(sr.Microphone.list_microphone_names())
+    mic = sr.Microphone(device_index=1)
+    with mic as source:
+        r.adjust_for_ambient_noise(source,duration=1)
+        audio = r.record(source,duration=4)
+    try:
+        result = r.recognize_google(audio, language='pl-PL')
+    except:
+        result = ''
     emitSignalOnFinished()
+    # print(f'recognize result: {result}')
+    return result
 
 
 def emitSignalOnFinished():
