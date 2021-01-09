3f95d94328613050e795469ff84da0a1431e7baf
Adam Osiowy <adamosiowy@wp.pl>
2021-01-09 23:06:41 +0100

    better interface

--- src/interface.py
+++ src/interface.py
@@ -12,11 +12,9 @@ class Ui_MainWindow(object):
         self.lineEdit.setObjectName("lineEdit")
         self.lineEdit.setDisabled(True)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(25, 70, 115, 25))
+        self.pushButton.setGeometry(QtCore.QRect(45, 70, 205, 25))
         self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(160, 70, 115, 25))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton.setToolTip('Kliknij aby rozpoznać głos')
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(25, 10, 100, 25))
         self.label.setObjectName("label")
@@ -35,6 +33,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Ludzka Konsola"))
-        self.pushButton.setText(_translate("MainWindow", "Start"))
-        self.pushButton_2.setText(_translate("MainWindow", "Stop"))
+        self.pushButton.setText(_translate("MainWindow", "Rozpoznaj polecenie"))
         self.label.setText(_translate("MainWindow", "Polecenie:"))
