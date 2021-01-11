5ef1051410d11efd3fb839f169811a8c4c42ecc1
Adam Osiowy <adamosiowy@wp.pl>
2021-01-11 16:10:02 +0100

    overall performance improvement

--- src/functions.py
+++ src/functions.py
@@ -11,17 +11,21 @@ import pyautogui
 import webbrowser
 import pdf2image
 import PyPDF2
-from pywinauto import Application
+import warnings
 import json
 import urllib.request
 import pafy
+import pywinauto
+import text_processing
+
+warnings.simplefilter("ignore", UserWarning)
 
 pathsOfApps = dict()
 
 
 def findTask(order):
     d = {
-        'podwyższyć': 'zwiększyć', 'zwiększyć': 'zwiększyć',
+        'podwyższyć': 'zwiększyć', 'podgłoś': 'zwiększyć', 'zwiększyć': 'zwiększyć',
         'pomniejszyć': 'zmniejszyć', 'pozmniejszać': 'zmniejszyć', 'zredukować': 'zmniejszyć', 'obniżyć': 'zmniejszyć',
         'zniżyć': 'zmniejszyć', 'zciszyć': 'zmiejszyć', 'zmniejszyć': 'zmniejszyć',
         'wyciszyć': 'wyciszyć',
@@ -107,11 +111,13 @@ def getListOfApps():
                 target = re.sub(r'[\|\\]', '/', target)
                 pathsOfApps.update({target.split('/')[-1][:-4].lower(): target})
             for i in lnk_files:
+                name = re.sub(r'[\|\\]', '/', i)
+                name = name.split('/')[-1][:-4].lower()
                 shortcut = shell.CreateShortCut(i)
                 target = shortcut.Targetpath
                 if target.endswith(".exe"):
                     target = re.sub(r'[\|\\]', '/', target)
-                    pathsOfApps.update({target.split('/')[-1][:-4].lower(): target})
+                    pathsOfApps.update({name: target})
         os.chdir(current_directory)
         return pathsOfApps
     else:
@@ -119,22 +125,33 @@ def getListOfApps():
 
 
 def runApp(args):
-    print(f'runApp args: {args}')
     paths = getListOfApps()
     print(paths)
     paths_keys = paths.keys()
     reg = '.?'.join(args.keys()) + '.*'
     for key in paths_keys:
         if re.match(reg, key, re.IGNORECASE):
-            subprocess.call(paths[key])
+            subprocess.Popen(paths[key])
             break
 
 
 def openFile(args):
+    current_dir = os.getcwd()
+    os.chdir('../res/')
     path = "../res/"
+    pop = ''
     for key in args.keys():
-        if args[key] == "SUBST":
-            os.startfile(path + key)
+        if re.match('plik', key):
+            pop = key
+    if len(pop)>0:
+        args.pop(pop)
+    name = ''.join(args.keys())
+    files = glob.glob('*.*')
+    for file in files:
+        if re.match(name, file, re.I):
+            os.startfile(file)
+            break
+    os.chdir(current_dir)
 
 
 def closeApp(args):
@@ -170,26 +187,39 @@ def makeScreenshot(args):
 
 def find(args):
     in_web = False
+    to_pop = set()
     for key in args.keys():
         if re.match('intern', key):
             in_web = True
-            args.pop(key)
+            to_pop.add(key)
+            continue
         if re.match('siec', key):
             in_web = True
-            args.pop(key)
+            to_pop.add(key)
+            continue
         if re.match('przeglądar', key):
             in_web = True
-            args.pop(key)
+            to_pop.add(key)
+            continue
         if re.match('necie', key):
             in_web = True
-            args.pop(key)
+            to_pop.add(key)
+            continue
         if re.match('stron', key):
             in_web = True
-            args.pop(key)
+            to_pop.add(key)
+            continue
         if re.match('plik', key):
-            args.pop(key)
+            to_pop.add(key)
+            continue
         if re.match('pdf', key):
-            args.pop(key)
+            to_pop.add(key)
+            continue
+        if re.match('inform', key):
+            to_pop.add(key)
+            continue
+    for i in to_pop:
+        args.pop(i)
     if in_web:
         findInWeb(args)
     else:
@@ -198,27 +228,36 @@ def find(args):
 
 def findInWeb(args):
     chrome_path = r"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-    url = "https://www.google.com/search?q=" + '+'.join(args.keys())
-    webbrowser.get(chrome_path).open(url)
+    if len(args) > 0:
+        tokens = text_processing.tokenize(' '.join(args.keys()))
+        info = text_processing.lematize(tokens)
+        url = "https://www.google.com/search?q=" + '+'.join(info)
+        print(url)
+        webbrowser.get(chrome_path).open(url)
 
 
 def findInPdfFiles(args):
-    current_dir = os.getcwd()
-    os.chdir('../res')
-    pdfFiles = glob.glob('*.pdf')
-    key = ' '.join(args.keys())
-    pagesToDisplay = []
-    for file in pdfFiles:
-        pdfReader = PyPDF2.PdfFileReader(file)
-        images = pdf2image.convert_from_path(file)
-        for i in range(pdfReader.getNumPages()):
-            page = pdfReader.getPage(i)
-            content = page.extractText()
-            if re.search(key, content, re.IGNORECASE):
-                pagesToDisplay.append(images[i])
-    for img in pagesToDisplay:
-        img.show()
-    os.chdir(current_dir)
+    print("find in files")
+    if len(args) > 0:
+        current_dir = os.getcwd()
+        os.chdir('../res')
+        pdfFiles = glob.glob('*.pdf')
+        key = ' '.join(args.keys())
+        print(key)
+        pagesToDisplay = []
+        for file in pdfFiles:
+            pdfReader = PyPDF2.PdfFileReader(file)
+            images = pdf2image.convert_from_path(file)
+            for i in range(pdfReader.getNumPages()):
+                page = pdfReader.getPage(i)
+                content = page.extractText()
+                print(content)
+                if re.search(key, content.lower(), re.IGNORECASE):
+                    print(f'found {key}')
+                    pagesToDisplay.append(images[i])
+        for img in pagesToDisplay:
+            img.show()
+        os.chdir(current_dir)
 
 
 def convertFromMp4ToMp3(args):
@@ -226,14 +265,15 @@ def convertFromMp4ToMp3(args):
     os.chdir('../res')
     mp4_files = glob.glob('*.mp4')
     reg = '.?'.join(args.keys()) + '.*'
+    print(reg)
     file_name = ''
     for file in mp4_files:
         res = re.search(reg, file, re.I)
         if res:
-            file_name = res.group()[:-4] + '.mp3'
-    command = '..\\res\\bin\\ffmpeg.exe -hide_banner -loglevel quiet -i .\\'
-    command += file_name[:-4] + '.mp4'
-    command += '.webm -vn -ab 128k -ar 44100 -y .\\'
+            file_name = file[:-4] + '.mp3'
+    command = '..\\res\\bin\\ffmpeg.exe -hide_banner -loglevel quiet -i '
+    command += '..\\res\\' + file_name[:-4] + '.mp4'
+    command += ' -vn -ab 128k -ar 44100 -y ..\\res\\'
     command += file_name
     print(f'convert command: {command}')
     subprocess.call(command, shell=True)
@@ -246,7 +286,7 @@ def downloadFromYT(args):
     video_id = ""
     api_key = "AIzaSyD_E7siJ1-4DSiqcQZxeMA90tuoTW92In8"
     try:
-        app = Application(backend='uia')
+        app = pywinauto.Application(backend='uia')
         app.connect(title_re=".*Chrome.*")
     except:
         print("can't connect to chrome!")
@@ -255,7 +295,7 @@ def downloadFromYT(args):
         url = dlg.child_window(title=element_name, control_type="Edit").get_value()
         res = re.search(r'(?<=watch\?v=).{11}|(?<=youtu.be/).{11}|(?<=y2u.be/).{11}', url, re.I)
         if res:
-            video_id = res
+            video_id = res.group()
     except:
         print("can't get link from chrome")
     api_url = f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={video_id}&key={api_key}"
